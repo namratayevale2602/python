ed65b3fffbf8de38b8073f3da0f3ca26880b8fe6
namratayevale <namratayevale90@gmail.com>
2025-07-07 22:35:14 +0530

    python

---
+++ tuple/tuple.py
@@ -0,0 +1,24 @@
+# Tuples are used to store multiple items in a single variable.
+
+# A tuple is a collection which is ordered and unchangeable and allow duplicate values..
+
+# Tuples are written with round brackets.
+
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+# To determine how many items a tuple has, use the len() function:
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))
+
+# A tuple with strings, integers and boolean values:
+tuple1 = ("abc", 34, True, 40, "male")
+
+# Using the tuple() method to make a tuple:
+thistuple = tuple(("apple", "banana", "cherry","banana")) # note the double round-brackets
+print(thistuple)
+
+
+
+#Other All Methods Same as list
